34254856b8b87cd9d22c72c625e5dd888cb9ed66
mmaria159 <macovei.maria1999@gmail.com>
2022-10-23 15:26:55 +0300

    add HM for section CMP

--- Home-Work/src/app/app.component.ts
+++ Home-Work/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
   title = 'Home-Work';
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
+  }
 }
+
